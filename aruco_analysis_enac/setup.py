d26eb83af599b4672d88e2292a715ae94ba9f255
jonathanTIE <jonathan_bleu@hotmail.fr>
2022-01-12 19:57:59 +0100

    merging

--- aruco_analysis_enac/setup.py
+++ aruco_analysis_enac/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'tf_transform = aruco_analysis_enac.main:main',
+            'aruco_pose_estimation = aruco_analysis_enac.aruco_pose_estimation:main',
             'detect_aruco = aruco_analysis_enac.detect_aruco:main',
         ],
     },
