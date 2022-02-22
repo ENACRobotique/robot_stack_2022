7a35168ab545f21f2f6c2bd91f270b5567744b13
jonathanTIE <jonathan_bleu@hotmail.fr>
2022-02-22 13:52:05 +0100

    a

--- aruco_analysis_enac/aruco_analysis_enac/analysis_aruco.py
+++ aruco_analysis_enac/aruco_analysis_enac/analysis_aruco.py
@@ -6,7 +6,7 @@ from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 
 import aruco_analysis_enac.aruco_calculations as calc
 from aruco_analysis_enac.aruco_storage import ArucosStorage
-from interfaces_enac.msg import _fiducials_poses
+from interfaces_enac.msg import _fiducials_poses, _object_marked
 
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
@@ -14,6 +14,7 @@ from tf2_ros import TransformBroadcaster
 
 
 FidPoses = _fiducials_poses.FiducialsPoses
+ObjectMarked = _object_marked.ObjectMarked
 
 class ArucoAnalysis(Node):
 
@@ -45,6 +46,7 @@ class ArucoAnalysis(Node):
         #self.object_poses_publisher = self.create_publisher(Aruco, 'object_poses', 10)
         #self.create_timer(movingArucoRate, self.object_poses_publisher)
 
+        self.object_marked_pub = self.create_publisher(ObjectMarked, 'object_marked', qos_profile_sensor_data)
         if self.debug_mode:
             self.diagnostics = self.create_publisher(
                 DiagnosticArray,
@@ -60,12 +62,12 @@ class ArucoAnalysis(Node):
 
         now = aruco_poses.header.stamp
         aurcosIdsIndex = [] #not reference
-        cameraPoseEnac = None
+        cameraPoseEnac = [] #store multiple camera pose estimated from all the reference aruco
         for i, id in enumerate(aruco_poses.marker_ids):
             if id in self.arucosStorage.reference_ids:
                 pose = calc.Pose.from_tvec_rvec(aruco_poses.tvecs[i], aruco_poses.rvecs[i])
                 MIDDLE = calc.Pose(1.50, 1.0, 0.0, (0,0,0)).transform_offset() #TODO : take from aruco storage
-                cameraPoseEnac = calc.get_camera_position( pose) #TODO : faire une fusion de données, là on se contente de prendre le dernier
+                cameraPoseEnac.append(calc.get_camera_position( pose))
 
                 calc.publish_pos_from_reference(self.tf_publisher, now, MIDDLE, 'origin', 'aruco')
                 calc.publish_pos_from_reference(self.tf_publisher, now, cameraPoseEnac, 'aruco', 'camera')
@@ -76,33 +78,22 @@ class ArucoAnalysis(Node):
             else:
                 aurcosIdsIndex.append(i)
 
-        if cameraPoseEnac == None:
+        if cameraPoseEnac == []:
             self.get_logger().info("missing reference aruco, can't estimate pose ! ")
             self.__send_diagnostics(DiagnosticStatus.ERROR, "missing reference aruco")
             return 
-        for i in aurcosIdsIndex:
-            marker_id = aruco_poses.marker_ids[i]
-            pose = calc.Pose.from_tvec_rvec(aruco_poses.tvecs[i], aruco_poses.rvecs[i])
-            table_pose = calc.table_pos_from_camera(pose, cameraPoseEnac)
-            self.get_logger().info(
-                f"{marker_id} is located on table at {table_pose}"
-            )
-            calc.publish_pos_from_reference(self.tf_publisher, now, pose.transform_offset(), 'camera', str(marker_id)+"_camera")
-            calc.publish_pos_from_reference(self.tf_publisher, now, table_pose, 'aruco', str(marker_id)+"_table")
+        for i, camera_pose in enumerate(cameraPoseEnac):
+            for i in aurcosIdsIndex:
+                marker_id = aruco_poses.marker_ids[i]
+                pose = calc.Pose.from_tvec_rvec(aruco_poses.tvecs[i], aruco_poses.rvecs[i])
+                table_pose = calc.table_pos_from_camera(pose, camera_pose)
+                self.get_logger().info(
+                    f"{marker_id} is located on table at {table_pose} according to reference at {camera_pose.position}"
+                )
+                calc.publish_pos_from_reference(self.tf_publisher, now, pose.transform_offset(), 'camera', str(marker_id)+"_camera_"+str(i))
+                calc.publish_pos_from_reference(self.tf_publisher, now, table_pose, 'aruco', str(marker_id)+"_table_"+str(i))
 
-            #calc.publish_pos_from_reference(self.tf_publisher, now, table_pose, 'aruco', str(marker_id)+"_table")
-
-            """
-            if marker_id == 13:
-                for x in range(2):
-                    x_rad = x*math.pi
-                    for y in range(2):
-                        y_rad = y*math.pi
-                        for z in range(2):
-                            z_rad = z*math.pi
-                            transf = pose.transform_offset(x_rad, y_rad, z_rad)
-                            calc.publish_pos_from_reference(self.tf_publisher, now, transf, 'camera', str(marker_id)+"_camera"+str(x)+str(y)+str(z))
-            """
+                #calc.publish_pos_from_reference(self.tf_publisher, now, table_pose, 'aruco', str(marker_id)+"_table")
 
     def __send_diagnostics(self, level, msg_txt):
         if not hasattr(self, 'diagnostics'):
@@ -120,7 +111,11 @@ class ArucoAnalysis(Node):
         msg.status = [reference]
         self.diagnostics.publish(msg)
 
-    def publish_arucos(self):
+    def publish_arucos(self, stamp, id, pose):
+        """Publish aruco poses in ObjectMarked """
+        cur_obj = ObjectMarked()
+        #cur_obj.header.stamp = rospy.Time.now()
+        self.object_marked_pub.publi
         pass
 
 
