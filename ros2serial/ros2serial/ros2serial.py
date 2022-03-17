c3de5b6be44661cb4a439d98cf24b99f15622735
KirrimK <KirrimK@users.noreply.github.com>
2022-03-17 19:15:08 +0100

    all messages from robot handled

--- ros2serial/ros2serial/ros2serial.py
+++ ros2serial/ros2serial/ros2serial.py
@@ -1,4 +1,4 @@
-from operator import truediv
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -7,16 +7,51 @@ from geometry_msgs.msg import Twist, Pose, Quaternion, TransformStamped, Transfo
 from nav_msgs.msg import Odometry
 from tf2_msgs.msg import TFMessage
 from tf2_ros import TransformBroadcaster
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
+
+from interfaces_enac import __sensorvalue
+
+SensorValue = __sensorvalue.SensorValue
 
 import serial
 import threading
 
-CMD_VEL = "s {} {}"
-ODOMETRY_REG = "o" #to be better defined
-ACTUATOR_REG = "a" #to be better defined
+CMD_VEL = "v {} {}"
+CMD_STOP = "s"
+CMD_ACTU = "a {} {}"
+CMD_DECL = "d"
+CMD_PID = "g {} {} {}"
+
+MESSAGE = "m"
+ODOM_MOTOR = "p"
+PERIPH_DECL = "b"
+CAPT_VAL = "c"
+
+#commandes à envoyer au serial:
+#v <int> <int>: commande de vitesse <linéaire * 1000> <omega * 1000>
+#s: arrêt du robot
+#a <id> <int> : ordre à un actionneur.
+#   id est deux caractères, le premier donnant le type d'actionneur (a pour AX12A, p pour pompe, e pour electroVanne, s pour servo)
+#   le deuxième est un chiffre d'identification.
+#d : demande de description des actionneurs (pas forcément utile: RoboKontrol?)
+#g [o/v] <int> <int> : changement des valeurs des PIDs (kp et ki)
+
+#commandes à récupérer du serial:
+#m <string>
+#p <double> <double> <double> <double> <double>: odométrie moteur <x> <y> <théta> <vlin> <vtheta>
+#b <string> <int> <int> <int> [R/RW] <string>: déclaration d'un actionneur (RW) ou d'un capteur (R). (pas forcément utile: RoboKontrol?) 
+#c <string> <int> : retour de capteur
+
+def quaternion_from_euler(roll, pitch, yaw):
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    return [qx, qy, qz, qw]
 
 class Ros2Serial(Node):
-    def __init__(self, port, timeout = 0.05, baudrate = 115200, rx_buffer_size=64, tx_buffer_size=64): # default buffer size like teensy
+    def __init__(self, port, timeout = 0.05, baudrate = 115200, rx_buffer_size=64, tx_buffer_size=64):
+        # default buffer size like teensy (TODO: voir si à garder pour stm32?)
         super().__init__("ros2serial")
         #paramétrage serial
         self.ser = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
@@ -24,8 +59,9 @@ class Ros2Serial(Node):
         self.thread_read = threading.Thread(target=self.serial_read)
         self.listen = True
         #paramétrage ROS
-        self.ros_publisher_odom = self.create_publisher(Odometry, 'topic', 10)
-        self.ros_publisher_actu = self.create_publisher(String, 'topic', 10)
+        self.ros_odom = self.create_publisher(Odometry, '/odom', 10)
+        self.ros_peripherals = self.create_publisher(SensorValue, '/peripherals', 10)
+        self.ros_diagnostics = self.create_publisher(DiagnosticArray, '/diagnostics', 10)
 
     def start_serial_read(self):
         self.thread_read.start()
@@ -39,25 +75,75 @@ class Ros2Serial(Node):
         while self.listen:
             message = self.ser.readLine()
             message = message.decode()
-            if len(message) != 0:
-                if message[0] == ODOMETRY_REG[0]:
+            if len(message) > 2:
+                if message[0] == MESSAGE:
+                    self.on_serial_msg(message[2:])
+                elif message[0] == ODOM_MOTOR:
                     self.on_serial_odom(message.split(' ')[1:])
-                if message[0] == ACTUATOR_REG[0]:
-                    self.on_serial_actu(message.split(' ')[1:])
+                elif message[0] == PERIPH_DECL:
+                    self.on_serial_periph(message)
+                elif message[0] == CAPT_VAL:
+                    self.on_serial_capt(message.split(' ')[1:])
+
+    def on_serial_msg(self, arg): #TODO: Tester
+        #convertir les infor reçues au format ROS2
+        msg = DiagnosticArray()
+        reference = DiagnosticStatus()
+        reference.level = 0
+        reference.name = "ros2serial_message"
+        reference.message = arg
+        reference.hardware_id = "stm32"
+        reference.values = []
+        msg.status = [reference]
+        self.ros_diagnostics.publish(msg)
 
     def on_serial_odom(self, args):
         #convertir les infor reçues au format ROS2
-        msg = Odometry(...) #TODO: args
-        ...
+        msg = Odometry() #TODO: args
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "map"
+        msg.child_frame_id = "robot"
+        #PoseWithCovariance
+        msg.pose.pose.position.x = float(args[0])
+        msg.pose.pose.position.y = float(args[1])
+        msg.pose.pose.position.z = 0.0
+        [qx, qy, qz, qw] = quaternion_from_euler(0.0, 0.0, float(args[2]))
+        msg.pose.pose.quaternion.x = qx
+        msg.pose.pose.quaternion.y = qy
+        msg.pose.pose.quaternion.z = qz
+        msg.pose.pose.quarernion.w = qw
+        #msg.pose.covariance = []
+        #TwistWithCovariance
+        msg.twist.linear.x = float(args[3])
+        msg.twist.linear.y = 0.0
+        msg.twist.linear.z = 0.0
+        msg.twist.angular.x = 0.0
+        msg.twist.angular.y = 0.0
+        msg.twist.angular.z = float(args[4])
+
         #envoyer les infos sur le bon topic
         self.ros_publisher_odom.publish(msg)
     
-    def on_serial_actu(self, args):
+    def on_serial_periph(self, arg):
         #convertir les infor reçues au format ROS2
-        msg = String(...) #TODO: args
-        ...
+        msg = DiagnosticArray()
+        reference = DiagnosticStatus()
+        reference.level = 0
+        reference.name = "ros2serial_periphDecl"
+        reference.message = arg
+        reference.hardware_id = "stm32"
+        reference.values = []
+        msg.status = [reference]
+        self.ros_diagnostics.publish(msg)
+
+    def on_serial_capt(self, args):
+        #convertir les infor reçues au format ROS2
+        msg = SensorValue() #TODO: args
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.sensor_name = args[0]
+        msg.value = int(args[1])
         #envoyer les infos sur le bon topic
-        self.ros_publisher_actu.publish(msg)
+        self.ros_peripherals.publish(msg)
 
 
     def serial_send(self, msg):
