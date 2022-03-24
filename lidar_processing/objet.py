464e0875912acf9f81b9f930fbb2c70cf7e6fc59
LilianEve <lilian.eve@hotmail.fr>
2022-03-24 11:33:24 +0100

    correction

--- lidar_processing/objet.py
+++ lidar_processing/objet.py
@@ -5,7 +5,7 @@ types_obj = ['Pylon', 'Tower', 'Friend', 'Foe', 'Unknown']
 class Objet:
     def __init__(self,typeobj)
         self.type=typeobj
-        self:relative_position
-        self:absolute_position
+        self.relative_position
+        self.absolute_position
        
         
