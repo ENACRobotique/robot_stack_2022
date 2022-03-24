658a93870e27a1a65216100489eaa8756d150517
LilianEve <lilian.eve@hotmail.fr>
2022-03-24 10:15:44 +0100

    ajout du publisher

--- lidar_processing/main.py
+++ lidar_processing/main.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 
 class lidarlocation(Node):
-     def __init__(self,name='Lidar_location'):
+    def __init__(self,name='Lidar_location'):
         super().__init__(name)
         self.subscription= self.create_subscription(
             LaserScan,
@@ -11,9 +11,31 @@ class lidarlocation(Node):
             self.listener_callback,
             10)
         self.subscription
+        self.publisher_=self.create_publisher(LaserScan,'filtered_scan',10)
+        
 
-     def listener_callback(self,msg):
-         self.get_logger().info(msg.ranges)    
+    def listener_callback(self,msg):
+        self.get_logger().info(msg.ranges)
+        msg_out = generate_filtered_message(msg,filter_out(msg))
+        self.get_logger().info(msg_out.ranges)
+        self.publisher_publish(msg_out) 
+             
+    def filter_out(message):
+        out = []
+        for i in range(0, len(message['ranges'])):
+            if message['ranges'][i] is not None :
+                if message['ranges'][i] > 3.6 :
+                    out.append(None)
+                else:
+                    out.append(message['ranges'][i])
+            else:
+                out.append(message['ranges'][i])
+        return out
+
+    def generate_filtered_message(message, filtered_data):
+        out = message
+        out['ranges'] = filtered_data
+        return out
 
 def main():
     rclpy.init()
