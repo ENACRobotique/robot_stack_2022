3de891468448eb8fe482c91857a7382d13a76204
ENAC Robotique <robotique.enac@gmail.com>
2022-03-30 18:39:03 +0200

    correction

--- lidar_location/lidar_location/Point.py
+++ lidar_location/lidar_location/Point.py
@@ -1,8 +1,8 @@
 
 class Point:
     def __init__(self):
-        self.angle = 0  # Radians
-        self.distance = 0  # en m
+        self.angle = 0.1  # Radians
+        self.distance = 0.1  # en m
         self.position_absolue = 0  # Position de l'element une fois determine
 
     # Retourne l'angle relatif du point en fonction de sa position et le pas de l'angle
