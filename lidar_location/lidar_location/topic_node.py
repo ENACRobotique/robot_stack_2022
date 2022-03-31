fa209933310068398b161e819ce70e9e1a5d346b
ENAC Robotique <robotique.enac@gmail.com>
2022-03-31 14:49:43 +0200

    dbug

--- lidar_location/lidar_location/topic_node.py
+++ lidar_location/lidar_location/topic_node.py
@@ -42,9 +42,11 @@ class lidarlocation(Node):
         msg_out = self.generate_filtered_message(msg, self.filter_out(msg))
         #msg_out = msg
         # self.get_logger().info(msg_out.angle_max)
-        triangulation = Object_list(msg_out)
+        #triangulation = Object_list(msg_out)
+
         self.get_logger().info(
-            f"debug triangles detectes : {triangulation.list_obj}")
+            f"debug triangles detectes : {msg_out.ranges}")
+        ##self.get_logger().info( f"debug triangles detectes : {triangulation.list_obj}")
         self.publisher_.publish(msg_out)
 
 
