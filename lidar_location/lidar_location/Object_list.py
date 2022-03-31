134ffc1ba9e5a9f9398cb8b8297f905c5f31fbdb
ENAC Robotique <robotique.enac@gmail.com>
2022-03-31 15:22:30 +0200

    dbug

--- lidar_location/lidar_location/Object_list.py
+++ lidar_location/lidar_location/Object_list.py
@@ -36,12 +36,10 @@ class Object_list:
             while (not self.is_break(position+1)) or (position < len(self.list_points) + start_position):
                 position += 1
                 list_pt_obj.append(self.list_points[position])
-                self.get_logger().info(
-                    f"position de calcul : {position}")
-                # at the end adds this list of points to an object
-                if list_pt_obj[0].distance > 0:  # Removes filtered points
-                    new_obj = Object(list_pt_obj)
-                    list_obj.append(new_obj)
+            # at the end adds this list of points to an object
+            if list_pt_obj[0].distance > 0:  # Removes filtered points
+                new_obj = Object(list_pt_obj)
+                list_obj.append(new_obj)
         return list_obj
 
     # Returns the first point in the list that is a break
