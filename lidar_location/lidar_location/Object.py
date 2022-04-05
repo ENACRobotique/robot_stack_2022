4cf7743cf12debe929173a5f1ee260d0046e0787
ENAC Robotique <robotique.enac@gmail.com>
2022-04-05 16:12:58 +0200

    gbug

--- lidar_location/lidar_location/Object.py
+++ lidar_location/lidar_location/Object.py
@@ -16,10 +16,13 @@ class Object:
     def _calculate_relative_center(self):
         new_p = Point()
         new_p.distance = 0
+        new_p.angle = 0
+
         for point in self.list_points:
-            new_p.distance += point.distance
-        new_p.distance /= len(self.list_points)
-        new_p.angle = self.list_points[math.floor(
-            len(self.list_points)/2)].angle
+            new_p.distance = new_p.distance + point.distance
+            new_p.angle = new_p.angle + point.angle
+
+        new_p.distance = new_p.distance / len(self.list_points)
+        new_p.angle = new_p.angle / len(self.list_points)
 
         return new_p
