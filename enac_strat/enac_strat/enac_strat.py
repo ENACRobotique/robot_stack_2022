f48841a1ce52a75a36d659f26b52619b6d5aef54
EnacRobotique <robotique.enac@gmail.com>
2022-05-11 19:30:54 +0200

    update nav

--- enac_strat/enac_strat/enac_strat.py
+++ enac_strat/enac_strat/enac_strat.py
@@ -1,4 +1,5 @@
-from enac_strat.enac_strat.conversions import z_euler_from_quaternions
+import math, time
+from enac_strat.enac_strat.conversions import z_euler_from_quaternions, quaternion_from_euler
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -9,22 +10,16 @@ from tf2_msgs.msg import TFMessage
 from tf2_ros import TransformBroadcaster
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 
-from interfaces_enac.msg import _periph_value, _pid
+from interfaces_enac.msg import _periph_value, _pid, _set_navigation
 
 PeriphValue = _periph_value.PeriphValue
 Pid = _pid.Pid
+SetNavigation = _set_navigation.SetNavigation
 
 #uses python-statemachine
 from statemachine import StateMachine, State
 
 #see for documentation https://github.com/fgmacedo/python-statemachine
-
-class StratStateMachine(StateMachine):
-    init = State('Init', initial=True) #état de préparation pré-tirette
-    outhome = State('Out_Home') #état de sortie de la zone de départ
-    
-    start = init.to(outhome)
-
 
 class Strategy(Node, StateMachine):
 
@@ -43,25 +38,30 @@ class Strategy(Node, StateMachine):
     vtheta = 0
     periphs = {}
 
+    goalx = 0
+    goaly = 0
+    goaltheta = 0
+
+    chrono = 0
+    end = 100
+
     def __init__(self):
         # default buffer size like teensy (TODO: voir si à garder pour stm32?)
         super().__init__("enac_strat")
-        self.state_machine = StratStateMachine(self.on_start)
 
         #paramétrage ROS
         self.ros_periph_pub = self.create_publisher(PeriphValue, '/peripherals', 10)
         self.ros_diag_pub = self.create_publisher(DiagnosticArray, '/diagnostics', 10)
         self.ros_vel_pub = self.create_subscription(Twist, '/cmd_vel', 10)
+        self.nav_pub = self.create_publisher(SetNavigation, 'navigation')
         
         self.ros_odom_sub = self.create_subscription(Odometry, '/odom', self.on_ros_odom, 10)
         self.ros_periph_sub = self.create_subscription(PeriphValue, '/peripherals', self.on_ros_periph, 10)
-
-        self.start_serial_read()
     
     def on_ros_periph(self, msg):
         if (msg.header.frame_id == "stm32"):#to prevent looping from self messages
             id = msg.periph_name[:2]
-            cmd = msg.value
+            cmd = int(msg.value)
             print("on_ros_periph "+str(id)+" "+str(cmd))
             self.periphs[id] = cmd
             self.check_transitions()
@@ -79,17 +79,66 @@ class Strategy(Node, StateMachine):
             self.vtheta = msg.twist.twist.angular.z
             self.check_transitions()
 
+    def is_at_goal(self, vlin_tol, vtheta_tol, distmax_mm):
+        return (abs(self.vlin) <= vlin_tol and
+                abs(self.vtheta) <= vtheta_tol and
+                math.sqrt((self.x - self.goalx)**2 + (self.y - self.goaly)**2) <= distmax_mm) #TODO: add condition sur theta si utile
+
     def check_transitions(self):
         try:
-            if self.state_machine.is_init:
-                if self.periphs["TI"] == 42:
-                    self.state_machine.start
-        except Exception as e:
-            pass
+            if self.chrono != 0 and (time.time() - self.chrono) > self.end - 10:
+                #go home
+                print("Strategy: il reste 10 secondes: retour à la maison")
+                pass
+            if self.is_init:
+                if self.periphs.get("TI") == 42:
+                    self.start
+            if self.is_outhome:
+                if self.is_at_goal(0.0001, 0.0001, 10):
+                    self.turn_palet #TODO: add transition and states
     
+        except Exception as e:
+            print("Strategy: crap in transition")
+            print(e)
+    
+    def send_nav_msg(self, nav_type: int, x: float, y: float, theta: float):
+        self.goalx = x
+        self.goaly = y
+        self.goaltheta = theta
+        msg = SetNavigation()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.navigation_type = nav_type
+        msg.pose.position.x = x
+        msg.pose.position.y = y
+        msg.pose.position.z = 0.0
+        [qx, qy, qz, qw] = quaternion_from_euler(0.0, 0.0, theta)
+        msg.pose.orientation.x = qx
+        msg.pose.orientation.y = qy
+        msg.pose.orientation.z = qz
+        msg.pose.orientation.w = qw
+        self.nav_pub.publish(msg)
+    
+    def send_periph_msg(self, id, cmd):
+        msg = PeriphValue()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "raspberry"
+        msg.periph_name = id
+        msg.value = cmd
+        #envoyer les infos sur le bon topic
+        self.ros_periph_pub.publish(msg)
+
+    def send_cmd_vel(self, vlin, vtheta):
+        msg = Twist()
+        msg.linear.x = vlin
+        msg.angular.z = vtheta
+        self.ros_vel_pub.publish(msg)
+
+
     #fonctions des transitions
     def on_start(self):
-        pass
+        print("Tirette détectée: start")
+        self.chrono = time.time()
+        self.send_nav_msg(1, 700, 1140, 0)
 
 
 def main(args=None):
